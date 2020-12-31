723741493696e38e137d33d787a7f98d151fc9cd
jrdmb <11788561+jrdmb@users.noreply.github.com>
2020-12-31 14:22:03 -0800

    10-Jul-2021 update

--- plugins/datasette_template_sql/__init__.py
+++ plugins/datasette_template_sql/__init__.py
@@ -2,9 +2,9 @@ from datasette import hookimpl
 
 
 @hookimpl
-def extra_template_vars(datasette, database):
+def extra_template_vars(datasette):
     async def execute_sql(sql, args=None, database=None):
-        database = database or database or next(iter(datasette.databases.keys()))
-        return (await datasette.execute(database, sql, args)).rows
+        db = datasette.get_database(database)
+        return (await db.execute(sql, args)).rows
 
     return {"sql": execute_sql}
